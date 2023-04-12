98d6645b2ed08c1673a8ddd6a5083d4be828ced2
hikari <tanimoto0709@gmail.com>
2023-04-13 07:55:36 +0900

    画像に対するアクションするためのパネルの生成処理と画像の左右入れ替え処理

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,122 +1,28 @@
 import '../scss/style.scss';
 
-const fileActionsDom = `
-  <div class="fileActions js-fileActions is-hide">
-    <svg
-      version="1.1"
-      id="_x32_"
-      xmlns="http://www.w3.org/2000/svg"
-      xmlns:xlink="http://www.w3.org/1999/xlink"
-      x="0px"
-      y="0px"
-      viewBox="0 0 512 512"
-      xml:space="preserve"
-      class="fileActions-close js-fileActions-close"
-    >
-      <g>
-        <polygon
-          points="326.253,147.826 256,218.08 185.747,147.826 147.819,185.747 218.072,256 147.819,326.253
-        185.747,364.182 256,293.929 326.262,364.182 364.19,326.253 293.928,256 364.19,185.738"
-        ></polygon>
-        <path
-          d="M437.018,74.99C390.76,28.699,326.624-0.008,256,0.009C185.385-0.008,121.24,28.699,74.982,74.99
-        C28.699,121.231-0.008,185.385,0,256c-0.008,70.615,28.699,134.76,74.982,181.01C121.24,483.301,185.385,512.009,256,511.991
-        c70.624,0.017,134.76-28.69,181.018-74.982C483.301,390.76,512.008,326.615,512,256C512.016,185.385,483.301,121.231,437.018,74.99
-        z M405.071,405.071c-38.222,38.181-90.774,61.739-149.071,61.739c-58.289,0-110.849-23.558-149.071-61.739
-        C68.747,366.84,45.198,314.281,45.181,256c0.016-58.28,23.566-110.848,61.748-149.071C145.151,68.739,197.711,45.19,256,45.19
-        c58.298,0,110.849,23.55,149.071,61.739c38.182,38.223,61.74,90.791,61.748,149.071
-        C466.811,314.281,443.253,366.84,405.071,405.071z"
-        ></path>
-      </g>
-    </svg>
-    <ul class="fileActions-list">
-      <li class="fileActions-itme js-file-moveRight">
-        <svg
-          version="1.1"
-          id="_x32_"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          x="0px"
-          y="0px"
-          viewBox="0 0 512 512"
-          xml:space="preserve"
-          class="fileActions-itme-icon"
-        >
-          <g>
-            <path
-              d="M499.436,225.905L295.858,24.536c-16.623-16.438-43.428-16.305-59.866,0.328
-  c-16.438,16.613-16.294,43.418,0.329,59.856l130.356,128.958H42.329C18.956,213.679,0,232.624,0,255.997
-  c0,23.383,18.956,42.328,42.329,42.328h324.347L236.321,427.273c-16.623,16.438-16.767,43.254-0.329,59.867
-  c16.438,16.622,43.243,16.766,59.866,0.328l203.578-201.368c8.044-7.963,12.564-18.792,12.564-30.102
-  C512,244.685,507.479,233.866,499.436,225.905z"
-            ></path>
-          </g>
-        </svg>
-        右に移動
-      </li>
-      <li class="fileActions-itme js-file-moveLeft">
-        <svg
-          version="1.1"
-          id="_x32_"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          x="0px"
-          y="0px"
-          viewBox="0 0 512 512"
-          xml:space="preserve"
-          class="fileActions-itme-icon"
-        >
-          <g>
-            <path
-              d="M469.672,213.675H145.324L275.68,84.727c16.623-16.438,16.767-43.253,0.329-59.866
-  c-16.438-16.624-43.243-16.767-59.867-0.328L12.566,225.901C4.52,233.863,0,244.691,0,256.003c0,11.312,4.52,22.13,12.566,30.093
-  l203.576,201.368c16.624,16.438,43.428,16.304,59.867-0.33c16.438-16.613,16.294-43.417-0.329-59.855L145.324,298.322h324.347
-  c23.374,0,42.328-18.945,42.328-42.319C512,232.62,493.045,213.675,469.672,213.675z"
-            ></path>
-          </g>
-        </svg>
-        左に移動
-      </li>
-      <li class="fileActions-itme fileActions-itme-remove js-file-delete">
-        <svg
-          version="1.1"
-          id="_x32_"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          x="0px"
-          y="0px"
-          viewBox="0 0 512 512"
-          xml:space="preserve"
-          class="fileActions-itme-icon"
-        >
-          <g>
-            <path
-              d="M77.869,448.93c0,13.312,1.623,25.652,5.275,35.961c4.951,13.636,13.475,23.457,26.299,26.297
-  c2.598,0.488,5.277,0.812,8.117,0.812h277.364c0.73,0,1.381,0,1.947-0.082c26.463-1.703,37.258-29.219,37.258-62.988
-  l11.121-269.324H66.748L77.869,448.93z M331.529,239.672h52.68v212.262h-52.68V239.672z M229.658,239.672h52.682v212.262h-52.682
-  V239.672z M127.789,239.672h52.762v212.262h-52.762V239.672z"
-            ></path>
-            <path
-              d="M368.666,89.289c0.078-2.028,0.242-4.059,0.242-6.09v-5.598c0-42.777-34.822-77.602-77.6-77.602h-70.701
-  c-42.778,0-77.6,34.824-77.6,77.602v5.598c0,2.031,0.162,4.062,0.326,6.09H28.721v62.582h454.558V89.289H368.666z M320.205,83.199
-  c0,2.113-0.242,4.141-0.648,6.09H192.361c-0.406-1.949-0.65-3.977-0.65-6.09v-5.598c0-15.91,12.986-28.898,28.897-28.898h70.701
-  c15.99,0,28.896,12.988,28.896,28.898V83.199z"
-            ></path>
-          </g>
-        </svg>
-        削除
-      </li>
-    </ul>
-  </div>
-`;
-
-// const uploadButton = document.getElementById('js-upload');
 const uploadInput = document.getElementById('js-upload-input');
 const saveButton = document.getElementById('js-save');
 const preview = document.getElementById('js-preview');
 let firstTime = true;
 
-// 同じ処理を書くから後でまとめておく 44行目
+const actionsAssets = {
+  left: {
+    hook: 'js-file-moveLeft',
+    imgName: 'arrow-left',
+    text: '左に移動',
+  },
+  right: {
+    hook: 'js-file-moveRight',
+    imgName: 'arrow-right',
+    text: '右に移動',
+  },
+  remove: {
+    hook: 'js-file-delete',
+    imgName: 'dust',
+    text: '削除',
+  },
+};
+
 const settingButton = document.querySelectorAll('.js-image-settings');
 settingButton.forEach((element) => {
   element.addEventListener('click', () => {
@@ -124,24 +30,26 @@ settingButton.forEach((element) => {
   });
 });
 
-// uploadButton.addEventListener('click', () => {
-//   console.log('upload');
-//   uploadInput.click();
-// });
-
-// 一度目にアップロードした画像がリセットされずに残っている
-// 一度目の状態を保存したままに追加ができるように修正する必要がある
+// TODO:一度目にアップロードした画像がリセットされずに残っている
+// TODO:一度目の状態を保存したままに追加ができるように修正する必要がある
 
 uploadInput.addEventListener('change', () => {
   const fileList = getUploadFileList();
+
+  // Chromeでは動くが他のブラウザの場合要注意
+  if (fileList.length === 0) return;
+
   const previewItems = createPreviewList(fileList);
+
+  // TODO:(要修正)初回の場合サンプル用のリストを削除、それ以降は追加できるように分岐
   if (firstTime) {
     preview.innerHTML = '';
     preview.appendChild(previewItems);
     firstTime = false;
   } else {
     let fragment = document.createDocumentFragment();
-    const sampleItems = document.querySelectorAll('#js-preview li');
+    // TODO: listがない時のデザインはCSSで制御してJSでの制御をやめる
+    const sampleItems = getPreviewList();
     for (let i = 0; i < sampleItems.length; i++) {
       const listItemDom = sampleItems[i];
       fragment.append(listItemDom);
@@ -150,27 +58,20 @@ uploadInput.addEventListener('change', () => {
     preview.appendChild(previewItems);
   }
 
-  const fileActions = document.querySelectorAll('.js-image-settings');
-  fileActions.forEach((element) => {
-    element.addEventListener('click', (event) => {
-      const clickedButton = event.currentTarget;
-      const target = clickedButton.nextElementSibling;
-      target.classList.remove('is-hide');
-    });
-  });
+  addEvents();
 });
 
 saveButton.addEventListener('click', () => {
   console.log('save');
 });
 
+function getPreviewList() {
+  const previewList = document.querySelectorAll('#js-preview > li');
+  return previewList;
+}
+
 function getUploadFileList() {
   const files = uploadInput.files;
-  // const fileData = new FileReader();
-  // fileData.onload = function () {
-  //   document.getElementById('js-preview').src = fileData.result;
-  // };
-  //fileData.readAsDataURL(hoge.files[0]);
   return files;
 }
 
@@ -178,18 +79,76 @@ function createPreviewList(fileData) {
   let fragment = document.createDocumentFragment();
 
   for (let i = 0; i < fileData.length; i++) {
-    const listItemDom = createListItem(fileData[i]);
+    const listItemDom = new createImageItem(fileData[i], i);
     fragment.append(listItemDom);
   }
-
   return fragment;
 }
 
-function createListItem(fileData) {
+function createActions() {
+  const div = document.createElement('div');
+  const list = document.createElement('ul');
+  const closeButton = document.createElement('img');
+
+  closeButton.width = '25';
+  closeButton.height = '25';
+  closeButton.classList.add('fileActions-close', 'js-fileActions-close');
+  closeButton.src = 'images/icon/close.png';
+
+  // TODO:actionsのlistItemの生成ループで回せるようにする classにしてメソッドとして持たせた方が良さそう
+  const actionsListItemRight = createActionsListItem(
+    actionsAssets.right.hook,
+    actionsAssets.right.imgName,
+    actionsAssets.right.text
+  );
+  const actionsListItemLeft = createActionsListItem(
+    actionsAssets.left.hook,
+    actionsAssets.left.imgName,
+    actionsAssets.left.text
+  );
+  const actionsListItemDust = createActionsListItem(
+    actionsAssets.remove.hook,
+    actionsAssets.remove.imgName,
+    actionsAssets.remove.text
+  );
+
+  list.appendChild(actionsListItemRight);
+  list.appendChild(actionsListItemLeft);
+  list.appendChild(actionsListItemDust);
+  list.classList.add('fileActions-list');
+
+  div.appendChild(closeButton);
+  div.appendChild(list);
+  div.classList.add('fileActions', 'js-fileActions', 'is-hide');
+  return div;
+}
+
+function createActionsListItem(jsHook, iconName, text) {
+  const li = document.createElement('li');
+  const img = document.createElement('img');
+  const p = document.createElement('p');
+
+  img.classList.add('fileActions-item-icon');
+  img.src = `images/icon/${iconName}.png`;
+  img.width = '15';
+  img.height = '15';
+
+  li.classList.add('fileActions-item', `${jsHook}`);
+
+  p.innerText = text;
+
+  li.appendChild(img);
+  li.appendChild(p);
+
+  return li;
+}
+
+function createImageItem(fileData, index) {
   const listItem = document.createElement('li');
   const thumbnail = document.createElement('img');
   const button = document.createElement('button');
   const gearIcon = document.createElement('img');
+  const actions = createActions();
 
   const fileReader = new FileReader();
   fileReader.readAsDataURL(fileData);
@@ -197,46 +156,142 @@ function createListItem(fileData) {
     thumbnail.src = fileReader.result;
   };
 
-  thumbnail.alt = 'アップロード画像';
+  thumbnail.alt = `アップロード画像${index}`;
   thumbnail.width = '150';
   thumbnail.height = '150';
 
   button.type = 'button';
   button.classList.add('image-settings', 'js-image-settings');
 
-  gearIcon.src = 'images/icon/gear.svg';
+  gearIcon.src = 'images/icon/gear.png';
   gearIcon.alt = '設定アイコン';
   gearIcon.width = '30';
   gearIcon.height = '30';
   gearIcon.classList.add('icon-gear');
 
   button.appendChild(gearIcon);
+  listItem.dataset.index = index;
+  listItem.classList.add('images-item');
   listItem.appendChild(thumbnail);
   listItem.appendChild(button);
-  listItem.insertAdjacentHTML('beforeend', fileActionsDom);
+  listItem.appendChild(actions);
 
+  return listItem;
+}
+
+function addEvents() {
+  // fileに対するアクションを実施するためのフックとなるelementの取得
   const fileMoveRight = document.querySelectorAll('.js-file-moveRight');
   const fileMoveLeft = document.querySelectorAll('.js-file-moveLeft');
   const fileDelete = document.querySelectorAll('.js-file-delete');
+  const fileActionClose = document.querySelectorAll('.js-fileActions-close');
+  const settings = document.querySelectorAll('.js-image-settings');
 
+  // ファイルアクションリストの表示
+  settings.forEach((element) => {
+    element.addEventListener('click', (event) => {
+      const clickedButton = event.currentTarget;
+      const target = clickedButton.nextElementSibling;
+      target.classList.remove('is-hide');
+    });
+  });
+
+  // 画像右移動
   fileMoveRight.forEach((element) => {
-    console.log(element);
-    element.addEventListener('click', () => {
-      console.log('move right');
+    element.addEventListener('click', (event) => {
+      event.stopImmediatePropagation();
+      const clickedButton = event.currentTarget;
+      handleImageMove(clickedButton, 'right');
     });
   });
 
+  // 画像左移動
   fileMoveLeft.forEach((element) => {
-    element.addEventListener('click', () => {
-      console.log('move left');
+    element.addEventListener('click', (event) => {
+      event.stopImmediatePropagation();
+      const clickedButton = event.currentTarget;
+      handleImageMove(clickedButton, 'left');
     });
   });
 
+  // 画像削除
   fileDelete.forEach((element) => {
-    element.addEventListener('click', () => {
+    element.addEventListener('click', (event) => {
       console.log('delete');
+      event.stopImmediatePropagation();
     });
   });
 
-  return listItem;
+  // アクションリストの非表示
+  fileActionClose.forEach((element) => {
+    element.addEventListener('click', (event) => {
+      console.log('close');
+      event.stopImmediatePropagation();
+    });
+  });
+}
+
+function handleImageMove(clickedButton, movement) {
+  // TODO: 選択要素と入れ替え先要素のindexを取得する関数として定義し直す
+  const imageItem = clickedButton.closest('.images-item');
+
+  // TODO：rightの文字列送りたくないけど一旦このまま実装
+  const [clickedObjectIndex, targetObjectIndex] = getObjectIndex(imageItem, movement);
+
+  // 入れ替え対象のitemがあるか確認
+  const listItems = getPreviewList();
+  if (!listItems[targetObjectIndex]) {
+    alert('対象の画像が見つかりません');
+    hideFileActionList(clickedButton);
+    return;
+  }
+
+  hideFileActionList(clickedButton);
+
+  // 入れ替えた後の配列を返す
+  const replacedListItem = replaceRightElement(listItems, clickedObjectIndex, targetObjectIndex);
+
+  // 入れ替えた後の要素を再描画
+  redrawingImageList(replacedListItem);
+}
+
+// 画像の位置を入れ替える
+function replaceRightElement(listItems, sourceIndex, targetIndex) {
+  const array = Array.from(listItems);
+  [array[targetIndex], array[sourceIndex]] = [array[sourceIndex], array[targetIndex]];
+
+  // data-indexの付け替え
+  [array[0].dataset.index, array[1].dataset.index] = [0, 1];
+  return array;
+}
+
+// 画像入れ替え後の再描画処理
+function redrawingImageList(array) {
+  let fragment = document.createDocumentFragment();
+  for (let i = 0; i < array.length; i++) {
+    const listItemDom = array[i];
+    fragment.append(listItemDom);
+  }
+  preview.innerHTML = '';
+  preview.appendChild(fragment);
+}
+
+// 画像に対する処理のリストを非表示にする
+function hideFileActionList(clickedButton) {
+  const target = clickedButton.closest('.js-fileActions');
+  target.classList.add('is-hide');
+}
+
+// 画像入れ替えの際に必要な要素のindex番号を返す
+function getObjectIndex(item, movement) {
+  const sourceObjectIndex = Number(item.dataset.index);
+  let targetObjectIndex = 0;
+
+  if (movement === 'right') {
+    targetObjectIndex = sourceObjectIndex + 1;
+  } else {
+    targetObjectIndex = sourceObjectIndex - 1;
+  }
+
+  return [sourceObjectIndex, targetObjectIndex];
 }
